73a4f2f99ed4d2a4128f8376821ab3042fc90102
Lucas <lucas_gomesdantas@hotmail.com>
2017-10-09 09:47:23 -0300

    Changing statement to ternary expression. Keep a clean code and looks more C++ style

--- empirical_analysis/src/main.cpp
+++ empirical_analysis/src/main.cpp
@@ -19,26 +19,18 @@ int main()
     //vector<int> max_size;
     //max_size.resize(1073741824); //2³⁰
 
-    //vector containing all the available sort functions
     vector<sort_functions> sort_list = { 
         insertion_sort, selection_sort, bubble_sort,
         quick_sort, merge_sort, shell_sort, radix_sort
     };
 
-    for(unsigned int i = 0; i < sort_list.size(); ++i)
+    for(unsigned int i = 0; i < sort_list.size(); ++i) //iterates over each function in vector
     {
-        //iterates over each function in vector
-        if(sort_list[i] == quick_sort or sort_list[i] == shell_sort)
-            sort_list[i]( A, A.begin(), A.end(), compare );
-        else    
-            sort_list[i]( A, A.begin(), A.end() - 1, compare );
-
+        auto end = sort_list[i] == quick_sort ? A.end() : A.end() - 1;
+        sort_list[i]( A, A.begin(), end, compare );
         for(unsigned int j = 0; j < A.size(); ++j) //checks if the ordenation is successfull
-            assert( (j + 1) == A[j] );
-
-        //shuffle so that the vector is unordered again
-        shuffle(A.begin(), A.end(), g);
-
+            assert( (j + 1) == A[j] );        
+        shuffle(A.begin(), A.end(), g); //shuffle so that the vector is unordered again
         cout << "-----------------------------------------" << endl;
     }
 }
