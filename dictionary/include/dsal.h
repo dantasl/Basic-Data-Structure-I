c28ca7c6fde554e1eece67e8f8a5549b10260a38
Lucas <lucas_gomesdantas@hotmail.com>
2017-12-08 02:47:23 -0300

    Several changes and bugfixes. Remove and insert workink. Min and Max too. Working now on successor and predecessor.

--- dictionary/include/dsal.h
+++ dictionary/include/dsal.h
@@ -23,8 +23,8 @@ namespace ac
 					auto data = DAL<Key, Data, KeyComparator>::mpt_Data[mid].id;
 					if ( not comp(data, _x) and not comp(_x, data)	)
 						return mid;
-					if ( comp(data, _x) ) left = mid + 1;
-					else right = mid - 1;
+					if ( comp(_x, data) ) right = mid - 1;
+					else left = mid + 1;
 				}
 
 				return -1;	
@@ -55,13 +55,13 @@ namespace ac
 				_s = data[find].info;
 
 				// Checks if element is the dictionary's back
-				if ( find == length ) { length -= 1;  return true; }				
+				if ( find == length - 1 ) { length -= 1;  return true; }				
 
 				// Otherwise, we need to delete and fill the gap
-				for ( auto i(length); i >= find; --i )
+				for ( auto i(find); i < length - 1; ++i )
 				{
-					data[i].id = data[i - 1].id;
-					data[i].info = data[i - 1].info;
+					data[i].id = data[i + 1].id;
+					data[i].info = data[i + 1].info;
 				}
 
 				length -= 1;
@@ -70,7 +70,7 @@ namespace ac
 
 			bool insert ( const Key & _newKey, const Data & _newInfo )
 			{
-				/*auto capacity = DAL<Key, Data, KeyComparator>::mi_Capacity;
+				auto capacity = DAL<Key, Data, KeyComparator>::mi_Capacity;
 				auto &length = DAL<Key, Data, KeyComparator>::mi_Length;
 				auto &data = DAL<Key, Data, KeyComparator>::mpt_Data;
 
@@ -85,105 +85,51 @@ namespace ac
 
 				// Checks if the dictionary can store new data and the client's input is unique
 				if ( length == capacity or _search(_newKey) != -1) return false;
-				
+
 				// We need to check where the input will be stored. For this, we need to find its immediate predecessor.
 				KeyComparator comp;
-				Key pre = min();
-				int pre_pos = 0; // Stores the position of the predecessor element
+				auto store_at = -1; // Where the new key will be inserted
 
-				// Finding _newKey's predecessor
+				// Finding _newKey's predecessor (it will be inserted right after it)
 				for(auto i(0); i != length; ++i)
 				{
-					if ( comp( data[i].id, _newKey ) and comp( pre, data[i].id ) )
-					{
-						pre = data[i].id;
-						pre_pos = i;
-						break;
-					}						
+					if ( comp( data[i].id, _newKey ) ) store_at = i;						
 				}
 
-				std::cout << "Inserting " << _newKey << " with predecessor " << data[pre_pos].id << std::endl;
-				for ( auto i(0); i != length; ++i ) std::cout << data[i].id << std::endl;
-
-				// Check if position to insert _newKey is at the dictionary's back
-				if ( pre_pos == length - 1 )
+				// Check if found position is at end of dictionary
+				if ( store_at == length - 1 )
 				{
 					data[length].id = _newKey;
 					data[length].info = _newInfo;
 					length += 1;
 					return true;
 				}
-
-				// Otherwise, we need to shift all data to the right and insert new element
-				for ( auto i(length - 1); i >= pre_pos ; --i )
+				else if ( store_at == - 1 ) // Means that its a insertion at the beginning of the dictionary
 				{
-					data[i].id = data[i - 1].id;
-					data[i].info = data[i - 1].info;
-				}
-
-				data[pre_pos + 1].id = _newKey;
-				data[pre_pos + 1].info = _newInfo;
-				length += 1;
-				return true;*/
-				auto &length = DAL<Key, Data, KeyComparator>::mi_Length;
-				auto &data = DAL<Key, Data, KeyComparator>::mpt_Data;
-				auto capacity = DAL<Key, Data, KeyComparator>::mi_Capacity;
-
-				auto t = _search(_newKey);
-				//<! check if has space in list or if empty list
-				if ( length == capacity or t != -1) return false;
-
-				
-				KeyComparator comp;
-				Key aux;
-				auto possible_pos = 0;
-				auto pos = -1;
-
-				//<! finds the correct position to insert
-				while ( possible_pos < length )
-				{
-					aux = data[possible_pos].id;
-					if ( comp( aux, _newKey) ) pos = possible_pos;
-
-					possible_pos++;
-				}
-
-				//<! check if the insertion is in the end
-				if ( pos == length-1 )
-				{
-					data[length].id = _newKey;
-					data[length].info = _newInfo;
-					length++;
-					return true;
-				}
-				else if ( pos == -1)
-				{
-					//<! open space
-					for ( auto i(length); i > pos+1; --i)
+					// Shift data to right to open gap for insertion
+					for ( auto i(length); i > store_at + 1; --i )
 					{
 						data[i].id = data[i-1].id;
 						data[i].info = data[i-1].info;
 					}
 					data[0].id = _newKey;
 					data[0].info = _newInfo;
-					length++;
+					length += 1;
 					return true;
-				}else
+				}
+				else // Means its a regular insertion at the "middle" of the dictionary
 				{
-					//<! open space
-					for ( auto i(length); i > pos+1; --i)
+					for ( auto i(length); i > store_at + 1; --i)
 					{
 						data[i].id = data[i-1].id;
 						data[i].info = data[i-1].info;
 					}
 
-						data[pos+1].id = _newKey;
-						data[pos+1].info = _newInfo;
-						length++;
-						return true;
+					data[store_at+1].id = _newKey;
+					data[store_at+1].info = _newInfo;
+					length += 1;
+					return true;
 				}
-				
-				return false;
 			}
 
 			Key min( void ) const
@@ -198,7 +144,7 @@ namespace ac
 				auto len = DAL<Key, Data, KeyComparator>::mi_Length;
 				if ( len == 0 )
 					throw std::out_of_range("Cannot access max element on an empty dictionary.");
-				return DAL<Key, Data, KeyComparator>::mpt_Data[len].id;
+				return DAL<Key, Data, KeyComparator>::mpt_Data[len - 1].id;
 			}
 
 			bool successor ( const Key & _x, Key & _y ) const
